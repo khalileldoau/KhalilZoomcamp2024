eac7c9a2e9ba0cc4103c71906fb8cfd0b5c43ac1
Khalil El Daou <khalil.m.eldaou@gmail.com>
2024-02-21 17:33:51 +0000

    homework finished

--- dbt/models/staging/stg_fhv_trips.sql
+++ dbt/models/staging/stg_fhv_trips.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 with 
 
 source as (
@@ -9,16 +15,31 @@ source as (
 renamed as (
 
     select
+
+        -- identifiers
+        {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
         dispatching_base_num,
-        pickup_datetime,
-        dropoff_datetime,
-        pulocationid,
-        dolocationid,
+        {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pulocationid,
+        {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dolocationid,
+
+        -- timestamps
+        cast(pickup_datetime as timestamp) as pickup_datetime,
+        cast(dropoff_datetime as timestamp) as dropoff_datetime,
+
+        -- trip info
         sr_flag,
         affiliated_base_number
 
     from source
-
 )
 
 select * from renamed
+where extract(year from pickup_datetime) = 2019
+
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=false) %}
+
+  limit 100
+
+{% endif %}
